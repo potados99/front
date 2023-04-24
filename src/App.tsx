5c2caebc12591955602e7885bf2754187c636774
potados99 <potados99@gmail.com>
2023-04-24 19:55:51 +0900

    refactor: 데이터 분리!

--- src/App.tsx
+++ src/App.tsx
@@ -3,109 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://www.potados.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          www.potados.com
-        </a>
-        <a
-          className="App-link"
-          href="https://duty.potados.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          duty.potados.com
-        </a>
-        <a
-          className="App-link"
-          href="https://home.potados.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          home.potados.com
-        </a>
-        <a
-          className="App-link"
-          href="https://cloud.potados.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          cloud.potados.com
-        </a>
-        <a
-          className="App-link"
-          href="https://collect.potados.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          collect.potados.com
-        </a>
-        <a
-          className="App-link"
-          href="https://chat.potados.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          chat.potados.com
-        </a>
-        <a
-          className="App-link"
-          href="https://blog.potados.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          blog.potados.com
-        </a>
-        <a
-          className="App-link"
-          href="https://tools.potados.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          tools.potados.com
-        </a>
-        <a
-          className="App-link"
-          href="https://sugang.potados.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          sugang.potados.com
-        </a>
-        <a
-          className="App-link"
-          href="https://shell.potados.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          shell.potados.com
-        </a>
-        <a
-          className="App-link"
-          href="https://desktop.potados.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          desktop.potados.com
-        </a>
-        <a
-          className="App-link"
-          href="https://browser.potados.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          browser.potados.com
-        </a>
-      </header>
-    </div>
-  );
+
+    const sites = [
+        'www.potados.com',
+        'duty.potados.com',
+        'home.potados.com',
+        'cloud.potados.com',
+        'collect.potados.com',
+        'chat.potados.com',
+        'blog.potados.com',
+        'tools.potados.com',
+        'sugang.potados.com',
+        'shell.potados.com',
+        'desktop.potados.com',
+        'browser.potados.com'
+    ];
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+
+                {sites.map(s =>
+                    <a
+                        className="App-link"
+                        href={`https://${s}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {s}
+                    </a>)
+                }
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
