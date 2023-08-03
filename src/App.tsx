ef06789d552df47cc479608866b356080a670b61
potados99 <potados99@gmail.com>
2023-08-03 22:38:33 +0900

    chore: add sites

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
 
-    const sites = [
-        'www.potados.com',
-        'duty.potados.com',
-        'home.potados.com',
-        'cloud.potados.com',
-        'collect.potados.com',
-        'chat.potados.com',
-        'blog.potados.com',
-        'tools.potados.com',
-        'sugang.potados.com',
-        'shell.potados.com',
-        'desktop.potados.com',
-        'browser.potados.com',
-        "flick.potados.com"
-    ];
+  const sites = [
+    "www.potados.com",
+    "duty.potados.com",
+    "aws.potados.com",
+    "home.potados.com",
+    "cloud.potados.com",
+    "collect.potados.com",
+    "chat.potados.com",
+    "bot.potados.com",
+    "blog.potados.com",
+    "tools.potados.com",
+    "sugang.potados.com",
+    "shell.potados.com",
+    "desktop.potados.com",
+    "browser.potados.com",
+    "flick.potados.com",
+    "focus.potados.com"
+  ];
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
 
-                {sites.map(s =>
-                    <a
-                        className="App-link"
-                        href={`https://${s}`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        {s}
-                    </a>)
-                }
+        {sites.map(s =>
+          <a
+            className="App-link"
+            href={`https://${s}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {s}
+          </a>)
+        }
 
-            </header>
-        </div>
-    );
+      </header>
+    </div>
+  );
 }
 
 export default App;
